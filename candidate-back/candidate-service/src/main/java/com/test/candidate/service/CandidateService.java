e2b0dd783098e0de22a89e01bba024808bd6b35e
Abdelali <Abdelali@192.168.50.167>
2015-09-16 16:26:40 +0200

    update

--- candidate-back/candidate-service/src/main/java/com/test/candidate/service/CandidateService.java
+++ candidate-back/candidate-service/src/main/java/com/test/candidate/service/CandidateService.java
@@ -1,14 +1,43 @@
 package com.test.candidate.service;
 
+import javax.annotation.PostConstruct;
+
 import org.springframework.stereotype.Component;
 
 /**
  * Created by oleg on 12/08/15.
  */
-
-public interface CandidateService {
-	List<Candidate> findAll();
-	void editCandidate(Candidate candidate);
-	void addCandidate(Candidate candidate);
-	void deleteCandidate(Candidate candidate);
+@Component
+@Path("/test")
+public class CandidateService {
+	
+	//The bean will inject by Spring IoC
+	@Autowired
+	private InterfaceCandidateService candidateService ;
+	
+	
+	@GET
+	@Path("/candidates")
+	@Produces(value={MediaType.APPLICATION_JSON,MediaType.APPLICATION_XML})
+	List<Candidate> findAll(){
+		return candidateService.findAll();;
+	}
+	
+	@POST
+	@Path("/candidates")
+	void addCandidate(@PathParam(value="candidate")Candidate candidate){
+		candidateService.addCandidate(Candidate candidate);
+	}
+	
+	@DELETE
+	@Path("/comptes/{candidate}")
+	void deleteCandidate(@PathParam(value="candidate")Candidate candidate){
+		candidateService.deleteCandidate(Candidate candidate);
+	}
+	
+	@PUT
+	@Path("/comptes/{candidate}")//I replace id by object
+	void editCandidate(@PathParam(value="candidate")Candidate candidate){
+		candidateService.editCandidate(Candidate candidate);
+	}
 }
